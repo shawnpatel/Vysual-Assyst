ed3fb932a6b984f11616b4b93a75553bcd500d53
Shawn Patel <shawnpatel8@att.net>
2018-12-16 21:16:54 -0800

    Initial Commit

---
+++ Vysual Assyst/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Vysual Assyst
+//
+//  Created by Shawn Patel on 12/16/18.
+//  Copyright © 2018 Shawn Patel. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
